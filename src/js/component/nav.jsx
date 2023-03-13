7a9f2364b0d5b4a09ecfcbf9643e04a42faa9dad
MGcode23 <marcrudo23@gmail.com>
2023-03-13 20:05:31 +0000

    mejorando en navbar

--- src/js/component/nav.jsx
+++ src/js/component/nav.jsx
@@ -3,11 +3,12 @@ import React from "react"
 function Nav (){
     return (
     <nav className="nav navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="navbar-nav ml-auto">
+  <div className="container fluid">
+  <a className="navbar-brand" href="#">Start Boobstrap</a>
   <a className="nav-link active" aria-current="page" href="#">Home</a>
   <a className="nav-link" href="#">About</a>
   <a className="nav-link" href="#">Services</a>
-  <a className="nav-link disabled">Contact</a>
+  <a className="nav-link" href="#">Contact</a>
   </div>
 </nav>
     )
